601ac36f69dbe2a28dab552090341cea8d968651
grasshopper42 <solite4686@gmail.com>
2023-09-27 19:19:53 +0900

    feat: Add Category CRUD

---
+++ lib/simon/catalog/category.ex
@@ -0,0 +1,18 @@
+defmodule Simon.Catalog.Category do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "categories" do
+    field :name, :string
+    field :parent_category_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(category, attrs) do
+    category
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+  end
+end
---
+++ priv/repo/migrations/20230927101929_create_categories.exs
@@ -0,0 +1,14 @@
+defmodule Simon.Repo.Migrations.CreateCategories do
+  use Ecto.Migration
+
+  def change do
+    create table(:categories) do
+      add :name, :string
+      add :parent_category_id, references(:categories, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create index(:categories, [:parent_category_id])
+  end
+end
